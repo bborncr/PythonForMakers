23e898718fd70b8b84eb40dfc43976a4fbbb58c5
Bentley Born <bentley@crcibernetica.com>
2024-10-30 10:09:34 -0700

    Update secret_santa.py

--- Session4/secret_santa.py
+++ Session4/secret_santa.py
@@ -1,7 +1,7 @@
 import random
 
 friends = ["James", "Emma", "Michael", "Olivia", "David"]
-cup = friends.copy()
+hay = friends.copy()
 
 # final_list = {"James":"Emma", "Emma":"Michael", "Michael":"Olivia"}  etc.
 final_list = {}
@@ -11,8 +11,8 @@ for friend in friends:
         name = random.choice(cup)
         while friend == name:
             print("You can't pick yourself...pick again!")
-            name = random.choice(cup)
-        cup.remove(name)
+            name = random.choice(hat)
+        hat.remove(name)
         final_list[friend] = name
         
 print(final_list)
