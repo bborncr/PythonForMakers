298362e855e572cf4c98daa6a50d506a37eb6d0a
Bentley Born <bentley@crcibernetica.com>
2024-10-30 13:42:32 -0700

    Update secret_santa.py

--- Session4/secret_santa.py
+++ Session4/secret_santa.py
@@ -7,13 +7,12 @@ hat = friends.copy()
 final_list = {}
 
 for friend in friends:
-    if len(cup) > 0:
-        name = random.choice(cup)
-        while friend == name:
-            print("You can't pick yourself...pick again!")
-            name = random.choice(hat)
-        hat.remove(name)
-        final_list[friend] = name
+    name = random.choice(hat)
+    while friend == name:
+        print("You can't pick yourself...pick again!")
+        name = random.choice(hat)
+    hat.remove(name)
+    final_list[friend] = name
         
 print(final_list)
             
