d5e52bfdca7e622386e18d18fcb9bab0069be491
Bentley Born <bentley@crcibernetica.com>
2024-11-27 12:58:43 -0800

    Update hangman.py

--- Session7/hangman.py
+++ Session7/hangman.py
@@ -15,16 +15,23 @@ while True:
     print("".join(show_word))
     guess = input("Guess a letter: ")
 
+    # check if previously guessed
     if guess in guessed:
         print(f"You already guessed `{guess}`")
         continue
     guessed.add(guess)
+    
+    # if the guess is equal to the letter replace the "-" with the letter
     for i in range(word_length):
         if word[i] == guess:
             show_word[i] = guess
+    
+    # check for win condition        
     if "-" not in show_word:
         print("You won!")
         break
+    
+    # too many tries
     tries = tries - 1
     if tries < 1:
         print("Too many tries! You lose!")
